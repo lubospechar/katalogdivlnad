ff601b8d93c1a639894550ef2a18f315d20a35fb
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-09-14 18:23:29 +0200

    Render descriptions as safe Markdown content
    Updated the `description` property to render translated content as safe HTML using `markdownify` and `mark_safe`. This ensures proper Markdown rendering while preventing issues with unsafe content.

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models import Q
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
-from django.utils.translation import get_language
 from django.core.exceptions import ValidationError
 from typing import Final
 from imagekit.models import ImageSpecField
