a915e8b8376277e282dab79ffa198eb45a08a67f
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-09-15 10:50:59 +0200

    Fix other_conflict_en in admin.

--- catalog/admin.py
+++ catalog/admin.py
@@ -399,7 +399,7 @@ class MeasureAdmin(admin.ModelAdmin):
                     "interconnection",
                     "conflict",
                     "other_conflict_cs",
-                    "other_conflicts_en",
+                    "other_conflict_en",
                     "references",
                     "contact_persons",
                     "dzes",
