65e50be61f4c4929843d9e1c90ba0c618d74524c
Luboš Pechar <lubos.pechar@prirodou.cz>
2025-12-02 14:01:46 +0100

    Add new fields with updated widgets in `FilterForm`
    Introduced fields: `size_scale`, `difficulty_of_implementation`, `quantification_scale`, and `time_horizon` to `FilterForm`. Updated `potential_scale` and new fields to use `CheckboxSelectMultiple` widget for improved filtering options.

--- catalog/forms.py
+++ catalog/forms.py
@@ -11,9 +11,37 @@ class FilterForm(forms.Form):
     )
 
     potential_scale = forms.ChoiceField(
-        label=_("Potential"),
+        label=_("Application potential"),
         choices=Measure.Size.choices,
-        widget=forms.RadioSelect,
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    size_scale = forms.ChoiceField(
+        label=_("Scale / extent"),
+        choices=Measure.Size.choices,
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    difficulty_of_implementation = forms.ChoiceField(
+        label=_("Implementation complexity"),
+        choices=Measure.Size.choices,
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    quantification_scale = forms.ChoiceField(
+        label=_("Impact quantification"),
+        choices=Measure.Size.choices,
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    time_horizon = forms.ChoiceField(
+        label=_("Impact time horizon"),
+        choices=Measure.Size.choices,
+        widget=forms.CheckboxSelectMultiple,
         required=False,
     )
 
